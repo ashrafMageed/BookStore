42f99088d142a87b59874b279d2332fc543444e2
Ashraf Mageed <ashrafmageed@ashrafs-mbp.home>
2018-08-24 08:34:16 +0100

    Adding in-memory database

--- Books.API/Controllers/BooksController.cs
+++ Books.API/Controllers/BooksController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Books.API.Controllers
 {
@@ -12,39 +14,63 @@ namespace Books.API.Controllers
     public class BooksController : ControllerBase
     {
         private static ConcurrentDictionary<string, Book> _books = new ConcurrentDictionary<string, Book>();
+        private readonly ApiContext _context;
+ 
+        public BooksController(ApiContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
         public ActionResult Get()
         {
-            return Ok(_books.Select(x => x.Value));
+            return Ok(_context.Books.ToList());
         }
 
         [HttpGet("{id}")]
         public ActionResult Get(string id)
         {
-            return Ok(_books.Where(x => x.Key == id).Select(x => x.Value));
+            var book = _context.Books.FirstOrDefault(x => x.Id == id);
+            
+            if(book == null)
+                return NotFound();
+
+            return Ok(book);
         }
 
         [HttpPost]
         public ActionResult Post([FromBody] Book book)
         {
-            if(_books.Keys.Any(x => x == book.Id))
+            if(_context.Books.Any(x => x.Id == book.Id))
                 return StatusCode(409);
 
-            _books.TryAdd(book.Id, book);
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
             return Created($"{Request.Path.ToUriComponent()}/{book.Id}", book);
         }
 
         [HttpPut("{id}")]
         public ActionResult Put(string id, [FromBody] Book book)
         {
-            return Ok(_books.AddOrUpdate(id, book, (k, v) => book));
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            return Ok();
         }
 
         [HttpDelete("{id}")]
         public ActionResult Delete(string id)
         {
-            return Ok(_books.TryRemove(id, out var book));
+            var book = _context.Books.FirstOrDefault(x => x.Id == id);
+            
+            if(book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+
+            return Ok();
         }
 
         [HttpPatch("{id}")]
@@ -53,7 +79,9 @@ namespace Books.API.Controllers
             if(jsonPatchDocument == null)
                 return BadRequest();
 
-            if(!_books.TryGetValue(id, out var bookToUpdate))
+            var bookToUpdate = _context.Books.FirstOrDefault(x => x.Id == id);
+            
+            if(bookToUpdate == null)
                 return NotFound();
 
             var updatableBook = bookToUpdate.GetUpdatableSection;    
@@ -62,9 +90,19 @@ namespace Books.API.Controllers
 
             bookToUpdate.MapFrom(updatableBook);
 
-            _books[id] = bookToUpdate;
+            _context.SaveChanges();
             return Ok();
         }
+    }
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+        }
+ 
+        public DbSet<Book> Books { get; set; }
+ 
+        public DbSet<Author> Authors { get; set; }
     }
 
     public class BookForUpdate
--- Books.API/Startup.cs
+++ Books.API/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Books.API.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +28,7 @@ namespace Books.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("BooksDb"));
             services.AddMvc(setupAction => {
                 
                 var inputFormatter = setupAction.InputFormatters.OfType<JsonInputFormatter>().FirstOrDefault();
